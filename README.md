246455aca7b2a23b5800e97f61308b7fe598f8ee
cstanger <ch.stanger@me.com>
2021-03-17 07:51:24 +0100

    Repo transfare commit

--- README.md
+++ README.md
@@ -1,11 +1,105 @@
-## My Project
+# serverless-plugin-lambda-insights
 
-TODO: Fill this README out!
+A Serverless Framework Plugin allowing to enable Lambda Insights
 
-Be sure to:
+![npm](https://img.shields.io/npm/v/serverless-plugin-lambda-insights)
+![npm](https://img.shields.io/npm/dw/serverless-plugin-lambda-insights)
 
-* Change the title in this README
-* Edit your repository description on GitHub
+Enables AWS Lambda Insights (https://aws.amazon.com/blogs/mt/introducing-cloudwatch-lambda-insights/) for the entire Serverless stack or individual functions.
+
+## Why using Lambda Insights
+
+> _CloudWatch Lambda Insights_ is a monitoring and troubleshooting solution for serverless applications running on AWS Lambda. The solution collects, aggregates, and summarizes system-level metrics including CPU time, memory, disk, and network. It also collects, aggregates, and summarizes diagnostic information such as cold starts and Lambda worker shutdowns to help you isolate issues with your Lambda functions and resolve them quickly.
+
+![AWS Documentation Example](https://docs.aws.amazon.com/lambda/latest/dg/images/lambdainsights-multifunction-view.png)
+
+---
+
+## Getting started
+
+### Installation
+
+`npm install --save-dev serverless-plugin-lambda-insights`
+
+add Plugin to your `serverless.yml` in the plugins section.
+
+### Minimal Usage
+
+Example `serverless.yml`:
+
+```yaml
+provider:
+  name: aws
+
+plugins:
+  - serverless-plugin-lambda-insights
+
+functions:
+  hello:
+    handler: handler.hello
+    lambdaInsights: true
+```
+
+### Functionality
+
+The plugin will enable Lambda Insights by adding a Lambda Layer ([see Layer Details and Versions](https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/Lambda-Insights-extension-versions.html)) and adding necessary permissions using the `arn:aws:iam::aws:policy/CloudWatchLambdaInsightsExecutionRolePolicy` as a AWS IAM Managed Policy.
+
+You can check in your AWS Console:
+go to AWS Lambda -> select your Lambda function -> Configuration tab -> Monitoring tools ->
+"CloudWatch Lambda Insights".
+If `lambdaInsights` validated to `true` for a function,
+the checkbox will be checked.
+
+### Usage
+
+Example `serverless.yml`:
+
+```yaml
+service: your-great-sls-service
+
+provider:
+  name: aws
+  stage: dev
+
+plugins:
+  - serverless-plugin-lambda-insights
+
+functions:
+  mainFunction: #inherits tracing settings from "provider"
+    lambdaInsights: true #turns on Lambda Insights for this function
+    handler: src/app/index.handler
+  secondFunction: #inherits tracing settings from "provider"
+    lambdaInsights: false #turns off Lambda Insights for this function, will overrule custom settings
+    handler: src/app/index.handler
+
+custom:
+  lambdaInsights:
+    defaultLambdaInsights: true #turn on Lambda Insights for all your function, if
+    attachPolicy: false #explicitly disable auto attachment Managed Policy.
+    lambdaInsightsVersion: 14 #specify the Layer Version
+```
+
+### Example
+
+You can find an example in the example folder of this repository. Run it with the following comment.
+
+`cd example; serverless deploy`
+
+This will deploy a hallo-world Lambda function with Lambda Insights enables.
+
+---
+
+## Want to contribute?
+
+This is your repo - just go head and create a pull request. See also [CONTRIBUTING] (CONTRIBUTING) for more introductions.
+
+Some open Ideas and Tasks:
+
+- [ ] Testing with Jest
+- [x] Add Toggle for auto policy attachment
+- [x] Add an example
+
+---
 
 ## Security
 
@@ -13,5 +107,4 @@ See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more inform
 
 ## License
 
-This library is licensed under the MIT-0 License. See the LICENSE file.
-
+This library is licensed under the MIT-0 License. See the [LICENSE](LICENSE) file.
---
+++ index.js
@@ -0,0 +1,155 @@
+'use strict';
+
+// Lambda Insight Layer Versions
+// see https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/Lambda-Insights-extension-versions.html
+const latestLayerVersion = 14;
+const layerVersions = [2, 11, 12, 14];
+const layerArn = (region, version) =>
+  `arn:aws:lambda:${region}:580247275435:layer:LambdaInsightsExtension:${version}`;
+
+const lambdaInsightsManagedPolicy = `arn:aws:iam::aws:policy/CloudWatchLambdaInsightsExecutionRolePolicy`;
+
+/**
+ * Serverless Lambda Insights Plugin - serverless-plugin-lambda-insights
+ * @class AddLambdaInsights
+ */
+class AddLambdaInsights {
+  /**
+   * AddLambdaInsights constructor
+   * This class gets instantiated with a serverless object and a bunch of options.
+   * @param  {object} serverless The serverless instance which enables access to global service config during
+   */
+  constructor(serverless) {
+    this.serverless = serverless;
+    this.service = this.serverless.service;
+    this.provider = this.serverless.getProvider('aws');
+
+    this.hooks = {
+      'before:package:setupProviderConfiguration': this.addLambdaInsights.bind(this),
+    };
+
+    serverless.configSchemaHandler.defineFunctionProperties('aws', {
+      properties: {
+        lambdaInsights: {type: 'boolean'},
+      },
+    });
+
+    serverless.configSchemaHandler.defineCustomProperties({
+      properties: {
+        lambdaInsights: {
+          defaultLambdaInsights: {type: 'boolean'},
+          attachPolicy: {type: 'boolean'},
+          lambdaInsightsVersion: {type: 'number'},
+        },
+      },
+    });
+  }
+
+  /**
+   * Check if Lambda Insights parameter is type of Boolean
+   * @param  {any} value Value to check
+   * @return {boolean} return input value if boolean
+   */
+  checkLambdaInsightsType(value) {
+    if (typeof value === 'boolean') {
+      return value;
+    } else {
+      throw new Error(
+          `LambdaInsights and DefaultLambdaInsights values must be set to either true or false.`,
+      );
+    }
+  }
+
+  /**
+   * Check if Lambda Insights Layer Version is valid
+   * @param  {any} value Value to check
+   * @return {boolean} return input value if available
+   */
+  checkLambdaInsightsVersion(value) {
+    if (typeof value === 'number' && layerVersions.includes(value)) {
+      return value;
+    } else {
+      throw new Error(`LambdaInsights layer version '${value}' does not exist.`);
+    }
+  }
+
+  /**
+   * Attach Lambda Layer conditionally to each function
+   * @param  {boolean} globalLambdaInsights global settings
+   * @param  {number} layerVersion global layerVersion settings
+   * @param  {boolean} attachPolicy global attachPolicy settings
+   */
+  addLambdaInsightsToFunctions(globalLambdaInsights, layerVersion, attachPolicy) {
+    if (typeof this.service.functions !== 'object') {
+      return;
+    }
+
+    let policyToggle = false;
+    Object.keys(this.service.functions).forEach((functionName) => {
+      const fn = this.service.functions[functionName];
+      const localLambdaInsights = fn.hasOwnProperty('lambdaInsights') ?
+        this.checkLambdaInsightsType(fn.lambdaInsights) :
+        null;
+
+      if (
+        localLambdaInsights === false ||
+        (localLambdaInsights === null && globalLambdaInsights === null)
+      ) {
+        return;
+      }
+
+      const fnLambdaInsights = localLambdaInsights || globalLambdaInsights;
+
+      if (fnLambdaInsights) {
+        // attach Lambda Layer
+        fn.layers = fn.layers || [];
+        fn.layers.push(
+            layerArn(
+                this.provider.getRegion(),
+                layerVersion || latestLayerVersion,
+            ),
+        );
+        policyToggle = true;
+      }
+    });
+    if (attachPolicy && policyToggle) {
+      // attach CloudWatchLambdaInsightsExecutionRolePolicy
+      this.service.provider.iamManagedPolicies =
+        this.service.provider.iamManagedPolicies || [];
+      this.service.provider.iamManagedPolicies.push(lambdaInsightsManagedPolicy);
+    }
+  }
+
+  /**
+   * Hook function to get global config value and executes addLambdaInsightsToFunctions
+   */
+  addLambdaInsights() {
+    const customLambdaInsights =
+      this.service.custom && this.service.custom.lambdaInsights;
+
+    const globalLambdaInsights =
+      customLambdaInsights && customLambdaInsights.defaultLambdaInsights ?
+        this.checkLambdaInsightsType(
+            customLambdaInsights.defaultLambdaInsights,
+        ) :
+        null;
+
+    const attachPolicy =
+      customLambdaInsights && customLambdaInsights.attachPolicy ?
+        this.checkLambdaInsightsType(
+            customLambdaInsights.attachPolicy,
+        ) :
+        true;
+
+    const layerVersion =
+      customLambdaInsights && customLambdaInsights.lambdaInsightsVersion ?
+        this.checkLambdaInsightsVersion(
+            customLambdaInsights.lambdaInsightsVersion,
+        ) :
+        null;
+
+    this.addLambdaInsightsToFunctions(globalLambdaInsights, layerVersion, attachPolicy);
+  }
+}
+
+module.exports = AddLambdaInsights;
